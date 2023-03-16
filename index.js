b18fdbce31c82aed2610e907fb90f7a5aaecfed4
marwanabdalla1 <marwanabdalla659@gmail.com>
2023-03-16 18:10:27 +0200

    samy

--- index.js
+++ index.js
@@ -4,12 +4,26 @@ const config = require('config')
 const helmet = require('helmet')
 const morgan = require('morgan') 
 const Joi = require('joi')
+
+
 const logger = require('./middleware/logger')
-const express = require('express')
-const { urlencoded } = require('express')
-const app = express()
 const courses = require('./routes/courses')
 const home = require('./routes/home')
+const express = require('express')
+
+
+const { urlencoded } = require('express')
+const app = express()
+
+require('dotenv').config();
+
+const mongoose = require('mongoose')
+const connectDB = require('./dbconnect')
+
+
+
+//Connect to the DB 
+ connectDB();
 
 
 //setting up the viewing template
@@ -43,7 +57,50 @@ dbdebug('Marwan is enabled')
 app.use(logger)
 
 
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [String],
+    date : { type: Date, default: Date.now },
+    isPublished : Boolean
+})
 
+const Course = mongoose.model ('Course', courseSchema)
+
+
+async function uploadCourse() {
+    const course = new Course ({
+        name: 'Angular course',
+        author: 'Mosh',
+        tags: ['angular', 'frontend'], 
+        isPublished: true 
+    
+    })
+
+    const result = await course.save()
+    console.log(result)
+}
+
+
+async function getCoursses(){
+    const courses = await Course
+    .find({isPublished: true, tags: 'backend' })
+    .sort({name: 1})
+    .select({name: 1, author: 1})
+    console.log(courses)
+}
+
+
+
+
+//get all published backend courses, sort them by their name, pick only their name and author and diplay them
+
+
+
+
+
+
+getCoursses()
 
 
 //routing fucntions
