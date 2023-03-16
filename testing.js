d26037effeed23cba69a4296e7d00df6010649d9
Nayer Kotry <nayer.kotry@outlook.com>
2023-03-16 13:36:15 +0200

    DUMPLINGZ

--- testing.js
+++ testing.js
@@ -1,3 +1,4 @@
 console.log("Hello Nk26");
 console.log("Hello maroo220022");
-console.log("Top ten deep conversations 2023 (18+ edition😳")
+console.log("Top ten deep conversations 2023 (18+ edition😳");
+console.log("هل تفهمنى يا قحبا");
